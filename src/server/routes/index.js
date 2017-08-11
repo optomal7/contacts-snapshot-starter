02d0442ab58d7268eeb04d480ab969b28f24edb9
Ryan Greenberg <hawk1138@gmail.com>
2017-08-10 17:23:37 -0700

    partial refactor

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -5,43 +5,19 @@ const contacts = require('./contacts')
 const users = require('./users');
 const DbContacts = require('../../db/contacts');
 
-
-
-router.use
-
-
 router.get('/', (request, response) => {
-  console.log('--------------');
-  console.log(request.session);
   if (!request.session.role) {
-    response.redirect('/login')
+    let error = 'Not logged in!'
+    response.render('login', {error})
+  } else {
+    DbContacts.getContacts()
+      .then((contacts) => {
+        let userinfo = request.session
+        console.log(contacts);
+        response.render('index', { contacts, userinfo })})
+      .catch( err => console.log('err', err) )
   }
-  DbContacts.getContacts()
-    .then((contacts) => {
-      let userinfo = request.session
-      console.log(contacts);
-      response.render('index', { contacts, userinfo })})
-    .catch( err => console.log('err', err) )
 })
-
-// router.get('/login', (request, response) => {
-//   response.render('login');
-// })
-//
-// router.post('/login', (request, response) => {
-//   dbUsers.getUser(request.body)
-//     .then((data) => {
-//       request.session.user = data
-//     })
-//     .then(() => console.log(request.session))
-//   response.redirect('/contacts');
-// })
-
-router.get('/signup', (request, response) => {
-  response.render('signup');
-})
-
-
 
 router.use('/contacts', contacts); // /contacts/search
 router.use('/login', users);
